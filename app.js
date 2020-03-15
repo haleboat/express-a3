e48940cb7d4444f9ee2d9bb1c69ab45895362a23
Alex Hale <59656730+haleboat@users.noreply.github.com>
2020-03-15 17:00:19 -0600

    donzo

--- app.js
+++ app.js
@@ -3,52 +3,68 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
+//Defining Image schema and requiring.
 const Image = require('./models/Images.js');
 
+// app setup
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
-var moment = require('moment');
+// using moment for footer copyright year.
+let moment = require('moment');
 app.locals.moment = moment;
 
+//setting view engine
 app.set('view engine', 'ejs');
 
+//setup pending DB connection
 const dbURI = process.env.MONGODB_URL;
 mongoose.connect(dbURI, {
   useUnifiedTopology: true,
   useNewUrlParser: true
 });
 
-// Connect to database. Mongoose handles the asynchronous aspects internally so we don't have to.
-var db = mongoose.connection;
+//mongoose handler for connection to DB
+let db = mongoose.connection;
 
-// Set a callback in case there's an error.
+//error callback
 db.on('error', function (error) {
   console.log(`Connection Error: ${error.message}`)
 });
-// Set a callback to let us know we're successfully connected
+
+//succesful callback
 db.once('open', function () {
   console.log('Connected to DB...');
 });
 
+//this is my homepage view template endpoint handler
 app.get('/', (req, res) => {
   res.render('pages/index');
 });
 
+//this is my gallery view template endpoint handler
 app.get('/images', (req, res) => {
-  res.render('pages/images', {
-    images
-  })
+  Image.find(function (error, result) {
+    if (error) {
+      return console.log(error);
+    }
+    res.render('pages/images', {
+      images: result
+    });
+  });
 });
 
+//this is my endpoint handler for clicking on an image in the gallery
 app.get('/images/:id', (req, res) => {
-  let photoID = `${req.params.id}`;
-  images.forEach(image => {
-    if (photoID === image.id.toString()) {
-      res.render('pages/image', {
-        image
-      });
+  Image.findOne({
+    id: req.params.id
+  }, function (error, result) {
+    if (error) {
+      return console.log(error);
     }
+    res.render('pages/image', {
+      images: result
+    });
   });
 });
 
